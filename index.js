e4ebea1ab1d5da723518d85a1c696cdf572bee3e
Alexey Elizarov <felex-ae@ya.ru>
2020-10-07 01:50:41 +0300

    refactor: update dependecies, reorder

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
  * https://github.com/devall/react-native-boilerplate
  */
 import 'react-native-gesture-handler';
+
 import { AppRegistry } from 'react-native';
 
-import App from './src/App';
 import { name as appName } from './app.json';
+import App from './src/App';
 
 AppRegistry.registerComponent(appName, () => App);
